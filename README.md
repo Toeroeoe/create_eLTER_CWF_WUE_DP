9a8a89af82553483331f51f4689712c13ac0d208
Christian Poppe <c.poppe@fz-juelich.de>
2025-09-04 16:40:57 +0200

    new time array construction, functional date attributes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # eLTER CWF WUE Data Product Creation
 
-This repository provides scripts and configuration for generating a Water-Use Efficiency (WUE) data product based on Community Land Model version 5 simulations with COSMO Reanalysis 6 forcings, using NetCDF files and custom selection logic. These are related to the Climate-Water-Food Reaseach Challenge of the eLTER PLUS project.
+This repository provides scripts and configuration for generating a Water-Use Efficiency (WUE) data product based on Community Land Model version 5 simulations with COSMO Reanalysis 6 forcings, using NetCDF files and custom selection logic. These are related to the Climate-Water-Food Reseach Challenge of the eLTER PLUS project.
 
 ## Code features
 
@@ -51,7 +51,7 @@ The output NetCDF file will be written by default to `out/CWF_WUE_DP/*out*.nc` b
 
 # Dataset notes
 
-The dataset is from processed CLM5-BGC simulations. The postprocessing involved resampling to 8-daily means, for compatibility with many contemporary remote sensing products. The Plant Functional Type (PFT) level variables resolve the averaged grid cell time series to each present PFT. The realtive coverage of the PFTs in each grid cell is indicated in the surface information variables (PCT_NAT_PFT), which have to be scaled by the share of natural vegetation land unit in the grid cell (PCT_NAT_VEG). Similarly, the present crop types (PCT_CFT, unirrigated or irrigated) have to be scaled with the present croplands land unit in the grid cell (PCT_CROP). Further information in Poppe Terán et al. 2025, in the CLM5 description paper and the CLM5 technical note.
+The dataset is from processed CLM5-BGC simulations. The postprocessing involved resampling to 8-daily means for compatibility with many contemporary remote sensing products. The Plant Functional Type (PFT) level variables resolve the averaged grid cell time series for each present PFT. The relative coverage of the PFTs in each grid cell is indicated in the surface information variables (PCT_NAT_PFT), which must be scaled by the share of natural vegetation land unit in the grid cell (PCT_NAT_VEG). Similarly, the present crop types (PCT_CFT, unirrigated or irrigated) must be scaled with the present cropland land unit in the grid cell (PCT_CROP). Further information is provided by Poppe Terán et al. 2025, in the CLM5 description paper and the CLM5 technical note.
 
 # Acknowledgements
 
@@ -59,13 +59,13 @@ This project has received funding from the European Union's Horizon 2020 researc
 
 # Citations
 
-Poppe Terán, Christian; Belleflamme, Alexandre; Naz S., Bibi; Hendricks-Franssen, Harrie-Jan; Vereecken, Harry; 2025, "European 3km CLM5-BGC Water-Use Efficiency Dataset", https://doi.org/10.26165/JUELICH-DATA/GROHKP, Jülich DATA, V1
+Poppe Terán, Christian; Belleflamme, Alexandre; Naz S., Bibi; Hendricks Franssen, Harrie-Jan; Vereecken, Harry, 2025, "European Water-Use Efficiency Dataset based on Community Land Model 5", https://doi.org/10.26165/JUELICH-DATA/UUCUVI, Jülich DATA, V1
 
 Poppe Terán, C., Naz, B.S., Vereecken, H., Baatz, R., Fisher, R.A., Hendricks Franssen, H.-J., 2025. Systematic underestimation of type-specific ecosystem process variability in the Community Land Model v5 over Europe. Geosci. Model Dev. 18, 287–317. https://doi.org/10.5194/gmd-18-287-2025
 
 
 Thörnig, P., 2021. JURECA: Data Centric and Booster Modules implementing the Modular Supercomputing Architecture at Jülich Supercomputing Centre. JLSRF 7, A182. https://doi.org/10.17815/jlsrf-7-182
 
-Lawrence, D.M., Fisher, R., Koven, C., Oleson, K., Swenson, S., Mariana Vertenstein, Ben Andre, Gordon Bonan, Bardan Ghimire, Leo van Kampenhout, Daniel Kennedy, Erik Kluzek, Ryan Knox, Peter Lawrence, Fang Li, Hongyi Li, Danica Lombardozzi, Yaqiong Lu, Justin Perket, William Riley, William Sacks, Mingjie Shi, Will Wieder, Chonggang Xu, Ashehad Ali, Andrew Badger, Gautam Bisht, Patrick Broxton, Michael Brunke, Jonathan Buzan, Martyn Clark, Tony Craig, Kyla Dahlin, Beth Drewniak, Louisa Emmons, Josh Fisher, Mark Flanner, Pierre Gentine, Jan Lenaerts, Sam Levis, L. Ruby Leung, William Lipscomb, Jon Pelletier, Daniel M. Ricciuto, Ben Sanderson, Jacquelyn Shuman, Andrew Slater, Zachary Subin, Jinyun Tang, Ahmed Tawfik, Quinn Thomas, Simone Tilmes, Francis Vitt, Xubin Zeng, 2018. CLM5 Documentation (Technical Note).
+Lawrence, D.M., Fisher, R., Koven, C., Oleson, K., Swenson, S., Mariana Vertenstein, Ben Andre, Gordon Bonan, Bardan Ghimire, Leo van Kampenhout, Daniel Kennedy, Erik Kluzek, Ryan Knox, Peter Lawrence, Fang Li, Hongyi Li, Danica Lombardozzi, Yaqiong Lu, Justin Perket, William Riley, William Sacks, Mingjie Shi, Will Wieder, Chonggang Xu, Ashehad Ali, Andrew Badger, Gautam Bisht, Patrick Broxton, Michael Brunke, Jonathan Buzan, Martyn Clark, Tony Craig, Kyla Dahlin, Beth Drewniak, Louisa Emmons, Josh Fisher, Mark Flanner, Pierre Gentine, Jan Lenaerts, Sam Levis, L. Ruby Leung, William Lipscomb, Jon Pelletier, Daniel M. Ricciuto, Ben Sanderson, Jacquelyn Shuman, Andrew Slater, Zachary Subin, Jinyun Tang, Ahmed Tawfik, Quinn Thomas, Simone Tilmes, Francis Vitt, Xubin Zeng, 2018. CLM5 Documentation (Technical Note). https://www2.cesm.ucar.edu/models/cesm2/land/CLM50_Tech_Note.pdf 
 
 Lawrence, D.M., Fisher, R.A., Koven, C.D., Oleson, K.W., Swenson, S.C., Bonan, G., Collier, N., Ghimire, B., Kampenhout, L., Kennedy, D., Kluzek, E., Lawrence, P.J., Li, F., Li, H., Lombardozzi, D., Riley, W.J., Sacks, W.J., Shi, M., Vertenstein, M., Wieder, W.R., Xu, C., Ali, A.A., Badger, A.M., Bisht, G., Broeke, M., Brunke, M.A., Burns, S.P., Buzan, J., Clark, M., Craig, A., Dahlin, K., Drewniak, B., Fisher, J.B., Flanner, M., Fox, A.M., Gentine, P., Hoffman, F., Keppel‐Aleks, G., Knox, R., Kumar, S., Lenaerts, J., Leung, L.R., Lipscomb, W.H., Lu, Y., Pandey, A., Pelletier, J.D., Perket, J., Randerson, J.T., Ricciuto, D.M., Sanderson, B.M., Slater, A., Subin, Z.M., Tang, J., Thomas, R.Q., Val Martin, M., Zeng, X., 2019. The Community Land Model Version 5: Description of New Features, Benchmarking, and Impact of Forcing Uncertainty. J. Adv. Model. Earth Syst. 11, 4245–4287. https://doi.org/10.1029/2018MS001583
--- src/create_dp.py
+++ src/create_dp.py
@@ -2,8 +2,32 @@ import os
 from netCDF4 import MFDataset, Dataset
 import yaml
 import numpy as np
+import xarray as xr
+import pandas as pd
 
-def get_nc_files_from_config(config_path: str, group: str) -> list[str]:
+def make_yearly_files(input_path: str, 
+                      output_dir: str) -> None:
+    
+    # Create output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+    
+    dataset = xr.open_dataset(input_path)
+
+    years = np.unique(dataset.time.dt.year.values)
+
+    for year in years:
+
+        print(f'Create yearly file for year {year}...\n')
+
+        yearly_data = dataset.sel(time=str(year))
+        yearly_data.to_netcdf(os.path.join(output_dir, 
+                                           f'{year}.nc'),
+                                           format='NETCDF4_CLASSIC',
+                                           unlimited_dims='time')
+        
+        
+def get_nc_files_from_config(config_path: str, 
+                             group: str) -> list[str]:
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
         if not isinstance(config, dict):
@@ -19,7 +43,8 @@ def get_nc_files_from_config(config_path: str, group: str) -> list[str]:
             nc_files.append(os.path.join(dir_path, file))
     return nc_files
 
-def open_mfdataset_from_config(config_path: str, group: str) -> MFDataset | Dataset:
+def open_mfdataset_from_config(config_path: str,
+                                group: str) -> MFDataset | Dataset:
     nc_files = get_nc_files_from_config(config_path, group)
 
     if not nc_files:
@@ -69,14 +94,16 @@ def save_selected_variables(ds: Dataset,
             else:
                 print(f"Warning: Variable {var_name} not found in dataset.")
 
-def main():
+def main() -> None:
     # Settings
     config_path = 'config.yaml'
     out_dir = 'out/CWF_WUE_DP/'
-    out_file = 'eLTER_CWF_WUE_DP_v1.0.nc'
+    version = 1.1
+    out_file = f'eLTER_CWF_WUE_DP_v{version}.nc'
     groups = ['coords', 'surface', 'forcings', 'grid', 'pft']
     fill_value = np.nan
-    
+    make_yearly = True
+
     # Directories
     os.makedirs(out_dir, exist_ok=True)
     out_path = os.path.join(out_dir, out_file)
@@ -87,102 +114,126 @@ def main():
     print("Loaded config:", config)
 
     # Read settings from config
-    if isinstance(config, dict):
-        variables_dict = config['variables'] if 'variables' in config else {}
-        dims_dict = config['dimensions'] if 'dimensions' in config else {}
-        names_dict = config['variable_names'] if 'variable_names' in config else {}
-        var_attrs_dict = config['variable_attributes'] if 'variable_attributes' in config else {}
-        var_factor_dict = config['variable_factor'] if 'variable_factor' in config else {}
-        var_dtype_dict = config['variable_dtype'] if 'variable_dtype' in config else {}
-        var_dims_dict = config['variable_dimensions'] if 'variable_dimensions' in config else {}
-        global_attrs_dict = config['global_attributes'] if 'global_attributes' in config else {}
-    else:
-         ValueError("Configuration file is empty or invalid.")
+    if not isinstance(config, dict):
+        raise ValueError("Configuration file is empty or invalid.")
 
-    # Make the dataset
-    with Dataset(out_path, 'w', format='NETCDF4_CLASSIC') as new_ds:
-        
-        # Create new NetCDF4 file
-        # Copy dimensions
-        for dim, props in dims_dict.items():
-            new_ds.createDimension(dim, size=(props['size'] if props['limited'] else None))
-        
-        # Create time coordinate variable and array
-        time_var = new_ds.createVariable('time', 'int32', 'time')
-        time_arr = (np.repeat(np.arange(0, 365, dims_dict['time']['day_step'])[:, np.newaxis], 
-                              dims_dict['time']['years'],
-                              axis=1)\
-                           + 365 * np.arange(0, dims_dict['time']['years'])).T.flatten()
+    variables_dict = config.get('variables', {})
+    dims_dict = config.get('dimensions', {})
+    names_dict = config.get('variable_names', {})
+    var_attrs_dict = config.get('variable_attributes', {})
+    var_factor_dict = config.get('variable_factor', {})
+    var_dtype_dict = config.get('variable_dtype', {})
+    var_dims_dict = config.get('variable_dimensions', {})
+    global_attrs_dict = config.get('global_attributes', {})
 
-        time_var[:] = time_arr
+    if not os.path.isfile(out_path):
+        # Make the dataset
+        with Dataset(out_path, 'w', format='NETCDF4_CLASSIC') as new_ds:
 
-        # Set time variable attributes
-        for attr, key in var_attrs_dict.get('time', {}).items():
-            time_var.setncattr(attr, key)
+            # Create new NetCDF4 file
+            # Copy dimensions
+            for dim, props in dims_dict.items():
+                new_ds.createDimension(dim, size=(props['size'] if props['limited'] else None))
 
-        # Copy global attributes
-        for attr, key in global_attrs_dict.items():
-            new_ds.setncattr(attr, key)
+            # Create time coordinate variable and array
+            time_var = new_ds.createVariable('time', 'int32', 'time')
+
+            time_s = pd.date_range(start=f"{dims_dict['time']['start_year']}-01-01", 
+                                   end=f"{dims_dict['time']['end_year']}-12-31", 
+                                   freq="D").to_series()
             
-        # Copy variables
-        for group in groups:
-            ds = open_mfdataset_from_config(config_path, group)
-            variables = variables_dict.get(group, [])
-            save_selected_variables(ds, 
-                                    new_ds, 
-                                    group, 
-                                    variables, 
-                                    names_dict, 
-                                    var_attrs_dict, 
-                                    var_factor_dict, 
-                                    var_dtype_dict,
-                                    var_dims_dict,
-                                    fill_value=fill_value)
-            print(f"Saved selected variables for group {group} to {out_path}")
+            time_groups = time_s.groupby(time_s.dt.year, 
+                                         group_keys=False)
 
-        # Calculate Ecosystem Water-Use Efficiency
-        if 'GPP' in new_ds.variables and 'ET' in new_ds.variables:
-            print("Both GPP and ET variables are present.")
-            gpp = new_ds.variables['GPP']
-            et = new_ds.variables['ET']
-            # Perform operations with gpp and et
-            ewue = np.where(et[:] != 0, gpp[:] / et[:], np.nan).astype('float32')
-            ewue = np.where(((ewue > 0) & (ewue < 30)), ewue, np.nan).astype('float32')
-            print(f"writing variable EWUE: EWUE, {gpp.dimensions}, float32")
-            new_var = new_ds.createVariable('EWUE', 'float32', gpp.dimensions, fill_value=fill_value)
-            new_var[:] = ewue
-            for attr, key in var_attrs_dict.get('EWUE', {}).items():
-                    new_var.setncattr(attr, key)
+            time_resampled = time_groups.resample(f"{dims_dict['time']['day_step']}D").asfreq()
 
-        # Calculate Transpiration Water-Use Efficiency
-        if 'GPP' in new_ds.variables and 'Tr' in new_ds.variables:
-            print("Both GPP and Tr variables are present.")
-            gpp = new_ds.variables['GPP']
-            tr = new_ds.variables['Tr']
-            # Perform operations with gpp and tr
-            twue = np.where(tr[:] != 0, gpp[:] / tr[:], np.nan).astype('float32')
-            twue = np.where(((twue > 0) & (twue < 30)), twue, np.nan).astype('float32')
-            print(f"writing variable TWUE: TWUE, {gpp.dimensions}, float32")
-            new_var = new_ds.createVariable('TWUE', 'float32', gpp.dimensions, fill_value=fill_value)
-            new_var[:] = twue
-            for attr, key in var_attrs_dict.get('TWUE', {}).items():
-                    new_var.setncattr(attr, key)
+            # Remove leap days if specified in config
+            if config['dimensions']['time']['leapday'] == False:
+                time_resampled = time_resampled[~((time_resampled.dt.month == 2) & (time_resampled.dt.day == 29))]
 
-        # Calculate Inherent Water-Use Efficiency
-        if 'GPP' in new_ds.variables and 'Tr' in new_ds.variables and 'VPD' in new_ds.variables:
-            print("GPP, Tr and VPD variables are present.")
-            gpp = new_ds.variables['GPP']
-            tr = new_ds.variables['Tr']
-            vpd = new_ds.variables['VPD']
-            # Perform operations with gpp and tr
-            gc = np.where(vpd[:] != 0, tr[:] / vpd[:], np.nan)
-            iwue = np.where(gc[:] != 0, gpp[:] / gc[:], np.nan).astype('float32')
-            iwue = np.where(((iwue > 0) & (iwue < 30)), iwue, np.nan).astype('float32')
-            print(f"writing variable IWUE: IWUE, {gpp.dimensions}, float32")
-            new_var = new_ds.createVariable('IWUE', 'float32', gpp.dimensions, fill_value=fill_value)
-            new_var[:] = iwue
-            for attr, key in var_attrs_dict.get('IWUE', {}).items():
-                    new_var.setncattr(attr, key)
+
+            time_since = (time_resampled - pd.Timestamp(f"{dims_dict['time']['start_year']}-01-01")).dt.days
+
+            time_arr = time_since.values.astype('int32')
+
+            time_var[:] = time_arr
+
+            time_var.setncattr('units', f"days since {dims_dict['time']['start_year']}-01-01")
+
+            # Set time variable attributes
+            for attr, key in var_attrs_dict.get('time', {}).items():
+                time_var.setncattr(attr, key)
+
+            # Copy global attributes
+            for attr, key in global_attrs_dict.items():
+                new_ds.setncattr(attr, key)
+            
+            new_ds.setncattr('history', f'Created on {np.datetime_as_string(np.datetime64("now"), unit="s")}')
+            new_ds.setncattr('date', np.datetime_as_string(np.datetime64("now")).format('%Y-%m-%d'))
+
+            # Copy variables
+            for group in groups:
+                ds = open_mfdataset_from_config(config_path, group)
+                variables = variables_dict.get(group, [])
+                save_selected_variables(ds, 
+                                        new_ds, 
+                                        group, 
+                                        variables, 
+                                        names_dict, 
+                                        var_attrs_dict, 
+                                        var_factor_dict, 
+                                        var_dtype_dict,
+                                        var_dims_dict,
+                                        fill_value=fill_value)
+                print(f"Saved selected variables for group {group} to {out_path}")
+
+            # Calculate Ecosystem Water-Use Efficiency
+            if 'GPP' in new_ds.variables and 'ET' in new_ds.variables:
+                print("Both GPP and ET variables are present.")
+                gpp = new_ds.variables['GPP']
+                et = new_ds.variables['ET']
+                # Perform operations with gpp and et
+                ewue = np.where(et[:] != 0, gpp[:] / et[:], np.nan).astype('float32')
+                ewue = np.where(((ewue > 0) & (ewue < 30)), ewue, np.nan).astype('float32')
+                print(f"writing variable EWUE: EWUE, {gpp.dimensions}, float32")
+                new_var = new_ds.createVariable('EWUE', 'float32', gpp.dimensions, fill_value=fill_value)
+                new_var[:] = ewue
+                for attr, key in var_attrs_dict.get('EWUE', {}).items():
+                        new_var.setncattr(attr, key)
+
+            # Calculate Transpiration Water-Use Efficiency
+            if 'GPP' in new_ds.variables and 'Tr' in new_ds.variables:
+                print("Both GPP and Tr variables are present.")
+                gpp = new_ds.variables['GPP']
+                tr = new_ds.variables['Tr']
+                # Perform operations with gpp and tr
+                twue = np.where(tr[:] != 0, gpp[:] / tr[:], np.nan).astype('float32')
+                twue = np.where(((twue > 0) & (twue < 30)), twue, np.nan).astype('float32')
+                print(f"writing variable TWUE: TWUE, {gpp.dimensions}, float32")
+                new_var = new_ds.createVariable('TWUE', 'float32', gpp.dimensions, fill_value=fill_value)
+                new_var[:] = twue
+                for attr, key in var_attrs_dict.get('TWUE', {}).items():
+                        new_var.setncattr(attr, key)
+
+            # Calculate Inherent Water-Use Efficiency
+            if 'GPP' in new_ds.variables and 'Tr' in new_ds.variables and 'VPD' in new_ds.variables:
+                print("GPP, Tr and VPD variables are present.")
+                gpp = new_ds.variables['GPP']
+                tr = new_ds.variables['Tr']
+                vpd = new_ds.variables['VPD']
+                # Perform operations with gpp and tr
+                gc = np.where(vpd[:] != 0, tr[:] / vpd[:], np.nan)
+                iwue = np.where(gc[:] != 0, gpp[:] / gc[:], np.nan).astype('float32')
+                iwue = np.where(((iwue > 0) & (iwue < 30)), iwue, np.nan).astype('float32')
+                print(f"writing variable IWUE: IWUE, {gpp.dimensions}, float32")
+                new_var = new_ds.createVariable('IWUE', 'float32', gpp.dimensions, fill_value=fill_value)
+                new_var[:] = iwue
+                for attr, key in var_attrs_dict.get('IWUE', {}).items():
+                        new_var.setncattr(attr, key)
+
+    if make_yearly:
+
+        make_yearly_files(out_path, os.path.join(out_dir, 'yearly'))
 
 if __name__ == "__main__":
     main()
